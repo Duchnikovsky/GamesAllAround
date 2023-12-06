be62b9f81826d1528e14b30cdfb913e639933c53
Duchnikovsky <filipduchnik@outlook.com>
2023-12-06 16:30:52 +0100

    fix: Inverted icon colors, fixed navbar auth query

--- client/src/components/Navbar/NavbarNav.tsx
+++ client/src/components/Navbar/NavbarNav.tsx
@@ -1,33 +1,28 @@
+import { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { ShoppingCart, User } from "lucide-react";
-import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
 
 export default function NavbarNav() {
   const navigate = useNavigate();
 
-  const useAuthQuery = () => {
-    const query = useQuery({
-      queryKey: ["auth-query"],
-      queryFn: async () => {
-        const url = `${import.meta.env.VITE_SERVER_URL}/auth/getAuth`;
-        const { data } = await axios.get(url, { withCredentials: true });
-        return data;
-      },
-      enabled: false,
-      retry: false,
-    });
+  const { refetch, isSuccess } = useQuery({
+    queryKey: ["auth-query"],
+    queryFn: async () => {
+      const url = `${import.meta.env.VITE_SERVER_URL}/auth/getAuth`;
+      const { data } = await axios.get(url, { withCredentials: true });
+      return data;
+    },
+    enabled: false,
+    retry: false,
+  });
 
-    useEffect(() => {
-      if (query.isSuccess) {
-        navigate("/profile");
-      }
-    }, [query.isSuccess]);
-
-    return query
-  };
+  useEffect(() => {
+    if (isSuccess) {
+      navigate("/profile");
+    }
+  }, [isSuccess]);
 
   return (
     <div className="flex items-center sm:gap-2">
@@ -49,7 +44,7 @@ export default function NavbarNav() {
       </Link>
       <div
         className="w-16 sm:w-20 h-14 bg-zinc-50 rounded-md flex flex-col items-center justify-center cursor-pointer hover:box-outline-sm"
-        onClick={() => useAuthQuery()}
+        onClick={() => refetch()}
       >
         <User size={30} strokeWidth={1.25} className="hidden sm:block" />
         <User size={20} strokeWidth={1.25} className="block sm:hidden" />
