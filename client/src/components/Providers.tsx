218b5077031d7de928ebf2f950b6410be0bac52c
Duchnikovsky <filipduchnik@outlook.com>
2023-12-18 14:22:56 +0100

    feat: add react redux package, created category global state

--- client/src/components/Providers.tsx
+++ client/src/components/Providers.tsx
@@ -1,5 +1,7 @@
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { FC, ReactNode } from "react";
+import { Provider } from "react-redux";
+import { store } from "../state/store";
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,7 +12,7 @@ const queryClient = new QueryClient();
 const Providers: FC<LayoutProps> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <Provider store={store}>{children}</Provider>
     </QueryClientProvider>
   );
 };
