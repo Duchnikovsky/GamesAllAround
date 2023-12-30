0802ccc62420ee3daefa86134191f998b5f5c430
Duchnikovsky <filipduchnik@outlook.com>
2023-12-30 19:59:55 +0100

    feat: add shadcn toast component and shadcn components controller, tailwind animate package

--- client/src/layout/Root.tsx
+++ client/src/layout/Root.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Modal from "../components/Modal/Modal";
+import { Toaster } from "../components/UI/shadcn/ui/toaster";
 
 export default function Root() {
   return (
@@ -8,6 +9,7 @@ export default function Root() {
       <div className="max-w-6xl mx-auto flex mt-14">
         <Navbar />
         <Modal />
+        <Toaster />
         <div className="mt-12 sm:mt-5 w-full">
           <Outlet />
         </div>
