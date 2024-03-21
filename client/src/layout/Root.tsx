69338afc95faa664fee82a314fe2e3acf33f7e8d
Duchnikovsky <filipduchnik@outlook.com>
2024-03-21 20:24:26 +0100

    feat: added react-icons liblary, added clash grotesk font as main site font

--- client/src/layout/Root.tsx
+++ client/src/layout/Root.tsx
@@ -1,12 +1,14 @@
+import { Outlet } from "react-router";
 import Navbar from "../components/Navbar/Navbar";
 
 export default function Root() {
   return (
     <div className='absolute top-0 inset-x-0 h-full bg-main bg-no-repeat bg-cover bg-fixed bg-center md:bg-top'>
-      <div className='absolute top-0 w-full h-full bg-blur'>
+      <div className='absolute top-0 w-full h-full bg-blur font-ClashGrotesk'>
         <div className="max-w-6xl mx-auto flex mt-14">
            <Navbar />
         </div>
+        <Outlet />
       </div>
     </div>
   )
