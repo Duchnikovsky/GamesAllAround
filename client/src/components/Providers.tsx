fec2534f012e5914b477924a9c366af37fe9a27f
Duchnikovsky <filipduchnik@outlook.com>
2024-03-26 15:37:53 +0100

    feat: added react redux state provider component covering whole application

---
+++ client/src/components/Providers.tsx
@@ -0,0 +1,10 @@
+import { Provider } from "react-redux";
+import { store } from "../states/store";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  return <Provider store={store}>{children}</Provider>;
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Root from "./layout/Root";
+import Providers from "./components/Providers";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-  }
-])
+  },
+]);
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <Providers>
+      <RouterProvider router={router} />
+    </Providers>
+  </React.StrictMode>
+);
