ad2f3e82a03acdeec59cb45ca140ff2c9b648540
Duchnikovsky <filipduchnik@outlook.com>
2024-03-27 17:01:16 +0100

    feat(navbar): modify session status to be based on useSession hook

--- client/src/components/Navbar/NavbarNav.tsx
+++ client/src/components/Navbar/NavbarNav.tsx
@@ -1,14 +1,14 @@
 import AuthNav from "./Nav/AuthNav";
 import ProfileNav from "./Nav/ProfileNav";
 import CartNav from "./Nav/CartNav";
+import useSession from "../../hooks/Session";
 
 export default function NavbarNav() {
-  // const [session, setSession] = useState<boolean>(true);
-  const session = true;
+  const session = useSession();
 
   return (
     <div className="flex items-center gap-4 pb-1">
-      {session ? (
+      {session.session.authenticated ? (
         <div className="flex gap-4">
           <CartNav />
           <ProfileNav />
