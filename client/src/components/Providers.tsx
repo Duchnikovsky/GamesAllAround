8346349f3e6668fc33680d2064a1b14a9ae81e34
Duchnikovsky <filipduchnik@outlook.com>
2024-03-27 17:00:03 +0100

    feat(package): add axios package and react-tanstack-query package, modify providers component by adding react query provider

--- client/src/components/Providers.tsx
+++ client/src/components/Providers.tsx
@@ -1,10 +1,17 @@
 import { Provider } from "react-redux";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { store } from "../states/store";
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
+const queryClient = new QueryClient();
+
 export default function Providers({ children }: ProvidersProps) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>{children}</Provider>
+    </QueryClientProvider>
+  );
 }
