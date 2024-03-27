b49d14d1cbcd28f249ed2600cfdd39af255407b8
Duchnikovsky <filipduchnik@outlook.com>
2024-03-27 12:35:05 +0100

    init(server): Initiate server side, install packages, config files, initiate prisma and construct database, create express back-end

---
+++ server/index.ts
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+import { PrismaClient } from "@prisma/client";
+export const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = process.env.APP_PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
