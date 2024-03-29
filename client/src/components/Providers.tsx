4fed1ef8dd70a9a8b8e52ef84c55654ae093f271
Duchnikovsky <filipduchnik@outlook.com>
2024-03-29 23:23:40 +0100

    feat(toast): add react-toastify liblary to handle notifications, add toast container to providers component

--- client/src/components/Providers.tsx
+++ client/src/components/Providers.tsx
@@ -1,6 +1,8 @@
 import { Provider } from "react-redux";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { store } from "../states/store";
+import { Flip, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 interface ProvidersProps {
   children: React.ReactNode;
@@ -11,7 +13,9 @@ const queryClient = new QueryClient();
 export default function Providers({ children }: ProvidersProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>{children}</Provider>
+      <Provider store={store}>
+        {children} <ToastContainer position="bottom-right" autoClose={3000} hideProgressBar={false} closeOnClick theme="dark" transition={Flip}/>
+      </Provider>
     </QueryClientProvider>
   );
 }
