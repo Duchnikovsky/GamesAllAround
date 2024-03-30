59756c86aa522c7104ac8f8d24d4e127acc11124
Duchnikovsky <filipduchnik@outlook.com>
2024-03-30 17:30:08 +0100

    feat(providers): add new query cache to manage and display query errors as notifications

--- client/src/components/Providers.tsx
+++ client/src/components/Providers.tsx
@@ -1,20 +1,38 @@
 import { Provider } from "react-redux";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import {
+  QueryClientProvider,
+  QueryClient,
+  QueryCache,
+} from "@tanstack/react-query";
 import { store } from "../states/store";
-import { Flip, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import { Flip, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  }),
+});
 
 export default function Providers({ children }: ProvidersProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        {children} <ToastContainer position="bottom-right" autoClose={3000} hideProgressBar={false} closeOnClick theme="dark" transition={Flip}/>
+        {children}{" "}
+        <ToastContainer
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          closeOnClick
+          theme="dark"
+          transition={Flip}
+        />
       </Provider>
     </QueryClientProvider>
   );
